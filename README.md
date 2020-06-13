f038a20bd95a040b747344fc04e661e0db30d0be
danielSbastos <daniel.bastos1710@gmail.com>
2020-06-13 11:54:30 -0300

    Introduce 3rd thread for test

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # pocket-conn-pool
 
-Pocket lib for handling connection pooling in Clojure.
-
-I just wanted to make a minimalistic lib to handle connection pooling in Clojure that uses no Java lib, like c3p0, and instead, only Clojure code.
+Pocket lib for handling connection pooling in Clojure, for learning purposes
--- src/pocket_conn_pool/core.clj
+++ src/pocket_conn_pool/core.clj
@@ -2,7 +2,6 @@
   (:require [clojure.java.jdbc :as jdbc]
             [clojure.core.async :refer [thread]]))
 
-
 (def connection-uri "postgres://twl_dev:@localhost:5432/twl_dev")
 (def max-connections 2)
 (def available (ref []))
@@ -40,15 +39,21 @@
      (alter in-use (partial remove #(= @pconn %)))
      (alter available conj @pconn))))
 
+
+;; ============== test ====================
+
 (defn thread-1 []
   (future
     (let [conn1 (get-connection)
-          _conn2 (get-connection)]
+          conn2 (get-connection)]
       (println "t1 => fetched conns")
       (println "t1 => sleeping 20 secs")
       (Thread/sleep 20000)
       (println "t1 => closed conn1")
-      (close-connection conn1))))
+      (close-connection conn1)
+      (Thread/sleep 2000)
+      (println "t1 => closed conn2")
+      (close-connection conn2))))
 
 (defn thread-2 []
   (future
@@ -59,7 +64,33 @@
       (println (time @conn3))
       (println "t2 => resolved conn"))))
 
+(defn thread-3 []
+  (future
+    (println "t3 => sleeping 13 secs")
+    (Thread/sleep 13000)
+    (let [conn4 (get-connection)]
+      (println "t3 => fetched conn")
+      (println (time @conn4))
+      (println "t3 => resolved conn"))))
+
 (defn t []
   (do
     (thread-1)
-    (thread-2)))
+    (thread-2)
+    (thread-3)))
+
+;; EXPECTED PRINTS
+;; "t1 => fetched conns"
+;; "t1 => sleeping 20 secs"
+;; "t2 => sleeping 10 secs"
+;; "t3 => sleeping 13 secs"
+;; ^--- order of these 4 may vary
+
+;; "t2 => fetched conn"
+;; "t3 => fetched conn"
+
+;; "t1 => closed conn1"
+;; "t2 => resolved conn"
+
+;; "t1 => closed conn2"
+;; "t3 => resolved conn"
