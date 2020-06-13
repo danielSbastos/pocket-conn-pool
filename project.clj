e93c0342c0cf2046f0816a16215339800c36c178
danielSbastos <daniel.bastos1710@gmail.com>
2020-06-13 00:14:58 -0300

    Refactor code and introduce waiting queue

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [postgresql/postgresql "8.4-702.jdbc4"]]
   :repl-options {:init-ns pocket-conn-pool.core})
--- src/pocket_conn_pool/core.clj
+++ src/pocket_conn_pool/core.clj
@@ -2,70 +2,64 @@
   (:require [clojure.java.jdbc :as jdbc]))
 
 (def connection-uri "postgres://twl_dev:@localhost:5432/twl_dev")
-(def max-connections 10)
-;; TODO: Can this be implemented in a deque? Do we need a "remove"? Can't it be a FIFO queue?
-(def available-conns (atom ()))
-(def alive-conns (atom ()))
-
-;; =========================
-
-(defn remove-alive-conns [conn]
-  (first (swap! alive-conns (partial remove #(= conn %)))))
-
-(defn pop-available-conns []
-  (first (swap! available-conns (partial drop 1))))
-
-(defn add-available-conns [conn]
-  (first (swap! available-conns conj conn)))
-
-(defn add-alive-conns [conn]
-  (first (swap! alive-conns conj conn)))
-
-(defn known-conn? [conn]
-  (not (empty? (filter #(= conn %) @alive-conns))))
-
-;; ========================
+(def max-connections 3)
+(def available (ref []))
+(def in-use (ref []))
+(def waiting (ref []))
 
 (defn get-connection []
-  (if (not-empty @available-conns)
-    (let [conn (pop-available-conns)]
-      (add-alive-conns conn))
-    (if (< (count @alive-conns) max-connections)
-      (let [conn (jdbc/get-connection connection-uri)]
-        (add-alive-conns conn))
-      (throw (Exception. "Max connections reached")))))
+  (let [conn (promise)]
+    (if (not-empty @available)
+      (dosync
+       (let [fetched-conn (first @available)]
+         (alter available (partial drop 1))
+         (alter in-use conj fetched-conn)
+         (deliver conn fetched-conn))
+       conn)
+      (if (< (count @in-use) max-connections)
+        (dosync
+         (let [fetched-conn (jdbc/get-connection connection-uri)]
+           (alter in-use conj fetched-conn)
+           (deliver conn fetched-conn))
+         conn)
+        (dosync
+         (alter waiting conj conn)
+         conn)))))
 
-(defn close-connection [conn]
-  (if (known-conn? conn)
-    (do
-      (add-available-conns conn)
-      (remove-alive-conns conn))
-    (throw (Exception. "Cannot remove unknown connection"))))
+(defn close-connection [pconn]
+  (if (> (count @waiting) 0)
+    (dosync
+     (alter in-use (partial remove #(= @pconn %)))
+     (let [w-conn (first @waiting)]
+       (alter waiting (partial drop 1))
+       (alter in-use conj w-conn)
+       (deliver w-conn @pconn)))
+    (dosync
+      (alter in-use (partial remove #(= @pconn %)))
+      (alter available conj @pconn))))
 
-;; ============================
-;; NEXT: Add option for waiting conn queue
-;; NEXT: Add option for timeout for waiting in conn queue
-
-(def test
-  (def conn1 (get-connection))
-  (def conn2 (get-connection))
-  (def conn3 (get-connection))
-  (def conn4 (get-connection))
-  (def conn5 (get-connection))
-  (def conn6 (get-connection))
-  (def conn7 (get-connection))
-  (def conn8 (get-connection))
-  (def conn9 (get-connection))
-  (def conn10 (get-connection))
-  (def conn11 (get-connection))
-  (close-connection conn1)
-  (close-connection conn2)
-  (close-connection conn3)
-  (close-connection conn4)
-  (close-connection conn5)
-  (close-connection conn6)
-  (close-connection conn7)
-  (close-connection conn8)
-  (close-connection conn9)
-  (close-connection conn10)
-  (close-connection conn11))
+(count @in-use)
+(count @available)
+(count @waiting)
+(def conn1 (get-connection))
+(def conn2 (get-connection))
+(def conn3 (get-connection))
+(def conn4 (get-connection))
+(def conn5 (get-connection))
+(def conn6 (get-connection))
+(def conn7 (get-connection))
+(def conn8 (get-connection))
+(def conn9 (get-connection))
+(def conn10 (get-connection))
+(def conn11 (get-connection))
+(close-connection conn1)
+(close-connection conn2)
+(close-connection conn3)
+(close-connection conn4)
+(close-connection conn5)
+(close-connection conn6)
+(close-connection conn7)
+(close-connection conn8)
+(close-connection conn9)
+(close-connection conn10)
+(close-connection conn11)
