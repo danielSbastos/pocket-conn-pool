95895e3c673eb48e6c38527fa8be9bf6300418dc
danielSbastos <daniel.bastos1710@gmail.com>
2020-06-22 23:11:15 -0300

    Building from scratch testing generator from specifications

--- test/pocket_conn_pool/core_test.clj
+++ test/pocket_conn_pool/core_test.clj
@@ -1,58 +1,45 @@
 (ns pocket-conn-pool.core-test
   (:require [clojure.test :refer [is]]
-            [clojure.test.check.generators :as gen]
             [pocket-conn-pool.core :as core]))
 
-(def gen-create
-  (gen/return :create))
+(def state [])
 
-(def gen-close
-  (gen/return :close))
+(def close-spec
+  {:precondition (fn [state] (some? state))
+   :args (fn [state] (first state))
+   :command (fn [conn] (core/close-connection conn))
+   :postcondition (fn [_] (identity true))
+   :next-state (fn [state result] (next state))})
 
-(defn commands-per-thread [thread-count]
-  (let [gen-commands (concat [] (gen/sample
-                                 (gen/one-of [gen-create gen-close])
-                                 (* 20 thread-count)))]
-    (partition (/ (count gen-commands) thread-count) gen-commands)))
+(def create-spec
+  {:precondition (fn [_] (identity true))
+   :args (fn [_] nil)
+   :command (fn [_] (core/create-connection))
+   :postcondition (fn [result]
+                    (is (and (<= (count @core/in-use) core/max-connections)
+                             (or (= :timeout result)
+                                 (= true (instance? org.postgresql.jdbc4.Jdbc4Connection result))))))
+   :next-state (fn [state result] (conj state result))})
 
-(def state (atom []))
+(defn run-spec! [spec state]
+  (when ((spec :precondition) state)
+    (when-let [result ((spec :command) ((spec :args) state))]
+      (when ((spec :postcondition) result)
+        ((:next-state spec) state result)))))
 
-(defn run-close []
-  (when-not (empty? @state)
-    (core/close-connection (first @state))
-    (swap! state next)))
+(defn gen-specs [close-spec create-spec]
+  (shuffle
+   (concat
+    (take (rand-int 200) (repeat create-spec))
+    (take (rand-int 200) (repeat close-spec)))))
 
-(defn run-create []
-  (let [conn (core/create-connection)]
-    (when-not (= :timeout conn)
-      (is (<= (count @state) core/max-connections))
-      (is (= (instance? org.postgresql.jdbc4.Jdbc4Connection conn)))
-      (swap! state conj conn))))
-
-(defn run-future [latch commands]
-  (future (do
-            (deref latch)
-            (doseq [cmd commands]
-              (case cmd
-                :create (run-create)
-                :close (run-close))))))
-
-(defn run-futures [latch parallel-size]
-  (let [cmds-per-thread (commands-per-thread parallel-size)]
-    (let [futures (into [] (map #(run-future latch %) cmds-per-thread))]
-      (deliver latch :go!)
-      futures)))
-
-(defn setup []
-  (doseq [conn (concat [] @core/in-use @core/available)]
-    (.close conn))
-  (dosync
-   (alter core/available (fn [_] []))
-   (alter core/in-use (fn [_] []))
-   (alter core/waiting (fn [_] []))))
+(defn run! [specs]
+  (reduce
+   (fn [state spec]
+     (run-spec! spec state))
+   []
+   specs)
+  true)
 
 (core/enable-ref-watchers)
-(dotimes [_ 1]
-  (setup)
-  (println "running commands")
-  (is (into [] (map #(deref %) (run-futures (promise) 2)))))
+(is (run! (gen-specs create-spec close-spec)))
