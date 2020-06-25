0a6f5f498ed04cc64c498843cc63c55e56cbf3f9
danielSbastos <daniel.bastos1710@gmail.com>
2020-06-25 11:47:56 -0300

    Improve test generation

--- test/pocket_conn_pool/core_test.clj
+++ test/pocket_conn_pool/core_test.clj
@@ -6,40 +6,65 @@
 
 (def close-spec
   {:precondition (fn [state] (some? state))
-   :args (fn [state] (first state))
-   :command (fn [conn] (core/close-connection conn))
-   :postcondition (fn [_] (identity true))
+   :command (fn [state] (core/close-connection (first state)))
+   :postcondition (fn [_ _] (identity true))
    :next-state (fn [state result] (next state))})
 
 (def create-spec
   {:precondition (fn [_] (identity true))
-   :args (fn [_] nil)
    :command (fn [_] (core/create-connection))
-   :postcondition (fn [result]
+   :postcondition (fn [_ result]
                     (is (and (<= (count @core/in-use) core/max-connections)
                              (or (= :timeout result)
                                  (= true (instance? org.postgresql.jdbc4.Jdbc4Connection result))))))
-   :next-state (fn [state result] (conj state result))})
+   :next-state (fn [state result]
+                 (when (not= :timeout result) (conj state result)))})
 
-(defn run-spec! [spec state]
-  (when ((spec :precondition) state)
-    (when-let [result ((spec :command) ((spec :args) state))]
-      (when ((spec :postcondition) result)
-        ((:next-state spec) state result)))))
+(defn execute-spec [spec]
+  (fn [key & args]
+    (apply (spec key) args)))
 
-(defn gen-specs [close-spec create-spec]
-  (shuffle
-   (concat
-    (take (rand-int 200) (repeat create-spec))
-    (take (rand-int 200) (repeat close-spec)))))
+(defn run-spec [execute-entry state]
+  (when (execute-entry :precondition state)
+    (when-let [result (execute-entry :command state)]
+      (when (execute-entry :postcondition state result)
+        (execute-entry :next-state state result)))))
 
-(defn run! [specs]
+(defn gen-specs []
+  (->> [create-spec close-spec]
+       (mapcat #(take (rand-int 100) (repeat %)))
+       shuffle))
+
+(defn run-specs [specs]
   (reduce
    (fn [state spec]
-     (run-spec! spec state))
+     (run-spec (execute-spec spec) state))
    []
    specs)
   true)
 
+(defn run-in-future [latch specs]
+  (future (do
+            (deref latch)
+            (run-specs specs))))
+
+(defn setup []
+  (doseq [conn (concat [] @core/in-use @core/available)]
+    (.close conn))
+  (dosync
+   (alter core/available (fn [_] []))
+   (alter core/in-use (fn [_] []))
+   (alter core/waiting (fn [_] []))))
+
+(defn execute-tests []
+  (let [latch (promise)]
+    (setup)
+    (let [test-futures (->> (range 0 1)
+                            (map (fn [_] (run-in-future latch (gen-specs))))
+                            (into []))]
+          (deliver latch :go!)
+          (doseq [f test-futures]
+            (deref f)))))
+
 (core/enable-ref-watchers)
-(is (run! (gen-specs create-spec close-spec)))
+(execute-tests)
